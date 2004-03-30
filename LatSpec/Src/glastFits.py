d85ada24c79a5f6721a88c1a4194a981126d7dea
focke <>
2004-03-30 22:32:59 +0000

    Added Data and Output directories.

--- LatSpec/Src/glastFits.py
+++ LatSpec/Src/glastFits.py
@@ -2,6 +2,12 @@
 ## @brief Stuff to write FITS files for GLAST.
 ## @author Warren Focke <focke@slac.stanford.edu> SLAC - GLAST I&T/SVAC
 
+#
+
+"""@brief Stuff to write FITS files for GLAST.
+
+"""
+
 import cfitsio
 
 
