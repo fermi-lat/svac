f3e3b83dec0dc87cf33a19398913f19cf6f4e1c7
focke <>
2004-04-07 15:45:44 +0000

    Added progress reports to VdGPHA.

--- LatSpec/Src/VdGPHA.py
+++ LatSpec/Src/VdGPHA.py
@@ -4,6 +4,8 @@
 ## @brief Create a PHA file from Van de Graaff data (or a Monte-Carlo).
 ## @author Warren Focke <focke@slac.stanford.edu> SLAC - GLAST I&T/SVAC
 
+import sys
+
 import Numeric as num
 
 import glastFits
@@ -20,14 +22,20 @@ epsilon = step / 2.0
 edges = num.arange(mainpage.eReconMin, mainpage.eReconMax+epsilon, step)
 
 # read data
+sys.stderr.write("Reading data from %s ... " % mainpage.specDataFile)
 data = tableIo.readTable(mainpage.specDataFile)
 eRecon = data[0]
+sys.stderr.write("ok.\n")
+
+sys.stderr.write("Calculating ... ")
 
 # histogram data
 binner = histogram.Histogram(edges)
 binner += eRecon
+sys.stderr.write("ok.\n")
 
 # output
+sys.stderr.write("Writing PHA to %s ... " % mainpage.phaFile)
 status = 0
 st, fptr = glastFits.createFile(mainpage.phaFile)
 status |= st
@@ -37,4 +45,5 @@ status |= glastFits.closeFile(fptr)
 
 if status:
     raise IOError, "CFITSIO problem."
+sys.stderr.write("ok.\n")
 
