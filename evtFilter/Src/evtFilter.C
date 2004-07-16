f0134a82a6ed9a38c810ea5fbbb1fe5f0be25241
focke <>
2004-07-16 23:17:58 +0000

    Started documentation.

--- evtFilter/Src/evtFilter.C
+++ evtFilter/Src/evtFilter.C
@@ -1,6 +1,8 @@
-//@file evtFilter.C
-//@brief Filter events from a digi or mc file.
-//@author Warren Focke <focke@slac.stanford.edu> SLAC - GLAST LAT I&T/SVAC
+/* 
+@file evtFilter.C
+@brief Filter events from a digi or mc file.
+@author Warren Focke <focke@slac.stanford.edu> SLAC - GLAST LAT I&T/SVAC
+*/
 
 #ifndef __CINT__ 
 #include <stdlib.h>
