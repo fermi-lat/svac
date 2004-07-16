dd5f3a985b4bc2c7f7aede04c2693300bcf6c5c9
focke <>
2004-07-16 23:09:06 +0000

    Moved source to Src directory.

--- evtFilter/Src/evtFilter.C
+++ evtFilter/Src/evtFilter.C
@@ -1,3 +1,6 @@
+//@file evtFilter.C
+//@brief Filter events from a digi or mc file.
+//@author Warren Focke <focke@slac.stanford.edu> SLAC - GLAST LAT I&T/SVAC
 
 #ifndef __CINT__ 
 #include <stdlib.h>
