53a8baa784ee25936951e76d03650ca7d88ae33f
focke <>
2004-07-31 00:12:28 +0000

    Wrappify digi and recon reports.

--- PipelineEM2/Src/genDigiTestReport.pl
+++ PipelineEM2/Src/genDigiTestReport.pl
@@ -2,41 +2,36 @@
 
 use strict;
 
-my $numArgs = $#ARGV + 1;
+use lib '/u/gl/dflath/glast/software/DPF/PDB/';
+use DPFProc;
 
-if($numArgs == 0) {
-    die "Missing run id as input argument";
-}
+#####################################################
+##
+##  BEGIN:  DON'T TOUCH STUFF BETWEEN THESE COMMENTS
+##
+#####################################################
 
-my $runId = $ARGV[0];
+my $proc = new DPFProc(@ARGV);
+my $inFiles = $proc->{'inFiles'};
+my $outFiles = $proc->{'outFiles'};
 
-my $rootDataDir = '/nfs/farm/g/glast/u01/temp/rootData/';
+#####################################################
+##
+##  END:  DON'T TOUCH STUFF BETWEEN THESE COMMENTS
+##
+#####################################################
 
-my $reportDir = $rootDataDir.$runId.'/report';
-# create report dir under rootDataDir if it does not exist 
-if(! (-d $reportDir) ) {
-    mkdir($reportDir) || die "can not make dir $reportDir, abort!";
-}
+my $digiRootFile = $inFiles->{'digi'};
+my $reconRootFile = 'noSuchFile';
+my $optionFile = $outFiles->{'jobOptions'};
+my $shellFile = $outFiles->{'script'};
+my $tarBall = $outFiles->{'tarBall'};
 
-my $runRootDir = $rootDataDir.$runId.'/rootFile';
+# gotta figure this out or get it from pieline DB
+my $reportDir = $??;
+# not sure if we really need this one
+my $timeStamp = $??;
 
-#search digi/recon root file
-opendir(DIR, $runRootDir) || die "can not find $runRootDir, abort!";
-my @files = readdir(DIR);
-my $timeStamp;
-foreach my $file (@files) {
-    if($file =~ /\_digi.root/ ) {
-	$timeStamp = substr($file, 0, length($file)-10);
-	last;
-    }
-}
-
-if(! defined($timeStamp)) {
-    die "digi root file can not be found, abort!";
-}
-
-my $digiRootFile = $runRootDir.'/'.$timeStamp.'_digi.root';
-my $reconRootFile = $runRootDir.'/'.$timeStamp.'_recon.root';
 my $texFile = 'refman.tex';
 my $dviFile = 'refman.dvi';
 my $psFile = 'refman.ps';
@@ -49,7 +44,6 @@ my $cmtDir = '/nfs/slac/g/svac/common/builds/TestReport/v2r0/cmt';
 my $exeDir = '/nfs/slac/g/svac/common/builds/TestReport/v2r0/rh9_gcc32';
 my $doxyFile = '/nfs/slac/g/svac/common/builds/TestReport/v2r0/src/ReportDoxyfile';
 
-my $optionFile = $reportDir.'/GentestReport_'.$timeStamp.'.txt';
 open(OPTFILE, ">$optionFile") || die "Can't open $optionFile for input, abortted!";
 print OPTFILE qq{$digiRootFile \n};
 print OPTFILE qq{$reconRootFile \n};
@@ -57,7 +51,6 @@ print OPTFILE qq{$reportDir \n};
 print OPTFILE qq{$timeStamp \n};
 close(OPTFILE);
 
-my $shellFile = $reportDir.'/GenTestReport_'.$timeStamp.'.scr';
 open(SHELLFILE, ">$shellFile") || die "Can't open $shellFile, abortted!";
 print SHELLFILE qq{#!/bin/csh \n \n};
 print SHELLFILE qq{unsetenv LD_LIBRARY_PATH \n};
@@ -70,6 +63,8 @@ print SHELLFILE qq{cd latex \n};
 print SHELLFILE qq{latex $texFile \n};
 print SHELLFILE qq{dvips -o $psFile $dviFile \n};
 print SHELLFILE qq{ps2pdf $psFile $pdfFile \n};
+print SHELLFILE qq{cd $reportDir \n};
+print SHELLFILE qq{tar cf $tarBall . \n};
 print SHELLFILE qq{cd $pwd};
 close(SHELLFILE);
 system("chmod +rwx $shellFile");
