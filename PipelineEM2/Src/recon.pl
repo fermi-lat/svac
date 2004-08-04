be6785e16e41f984d9d5395bbb8d02ef5ea26f56
focke <>
2004-08-04 05:23:52 +0000

    Finished interface separation for recon.

--- PipelineEM2/Src/recon.pl
+++ PipelineEM2/Src/recon.pl
@@ -2,30 +2,7 @@
 
 use strict;
 
-use lib '/u/gl/dflath/glast/software/DPF/PDB/';
-use DPFProc;
-
-#####################################################
-##
-##  BEGIN:  DON'T TOUCH STUFF BETWEEN THESE COMMENTS
-##
-#####################################################
-
-my $proc = new DPFProc(@ARGV);
-my $inFiles = $proc->{'inFiles'};
-my $outFiles = $proc->{'outFiles'};
-
-#####################################################
-##
-##  END:  DON'T TOUCH STUFF BETWEEN THESE COMMENTS
-##
-#####################################################
-
-my $digiRootFile = $inFiles->{'digi'};
-my $shellFile = $outFiles->{'script'};
-my $jobOptionFile = $outFiles->{'jobOptions'};
-my $reconRootFile = $outFiles->{'recon'};
-my $meritRootFile = $outFiles->{'merit'};
+my ($digiRootFile, $shellFile, $jobOptionFile, $reconRootFile, $meritRootFile) = @argv; 
 
 open(SHELLFILE, ">$shellFile") || die "Can't open $shellFile, abortted!";
 print SHELLFILE "#!/bin/sh \n \n";
