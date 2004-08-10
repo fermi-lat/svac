ac0cbfc52bf7575d4098880a28cbf75e96fc1fc0
focke <>
2004-08-10 21:46:34 +0000

    Fixed glastRoot in ldf2digi.

--- PipelineEM2/Src/ldfToDigi.pl
+++ PipelineEM2/Src/ldfToDigi.pl
@@ -22,7 +22,7 @@ $0 running with options:
   exe :          $exe
 EOT
 
-my $glastRoot = "";
+my $glastRoot = "/afs/slac.stanford.edu/g/glast";
 my $glastScript = "$glastRoot/ground/scripts/user.cshrc";
 
 open(SHELLFILE, ">$shellFile") || die "Can't open $shellFile, abortted!";
