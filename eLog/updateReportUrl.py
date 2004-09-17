0f11fd86b46cb85c928b7161b46a58cb4c43ca14
focke <>
2004-09-17 23:11:54 +0000

    Continue work un report URL updater.

--- eLog/updateReportUrl.py
+++ eLog/updateReportUrl.py
@@ -23,8 +23,8 @@ location = {"config": "config/v0r0/ConfigTables.html",
             "recon": "calibv0/recon_report/v0r0/html/index.html"}
 
 urlKey = {"config": "ConfigReportUrl",
-          "digi": "",
-          "recon": "",}
+          "digi": "DigiReportUrl",
+          "recon": "ReconReportUrl",}
 
 failCode = 1
 
@@ -44,34 +44,25 @@ if reportType not in location:
 
 #open connection to oracle database
 db = DCOracle2.connect('GLAST_CAL/9square#')
-
 c = db.cursor()
 
 # determine whether run is already in the database
 sqlStr = 'select * from eLogReport where RunId = %s' % runId
-
 c.execute(sqlStr)
-
 result = c.fetchone()
-
 if(result == None):
     print 'run %s does not exist in the database, abortted!' % runId
     db.close()
     sys.exit(failCode)
     
 # construct URL string for config report generated from online snapshots
-    
 reportUrl = "ftp://ftp-glast.slac.stanford.edu/%s/%s/%s" % \
             (rootDataDir, runId, location[reportType])
     
 # construct sql string to input data into oracle database.
-# in python, \' is used to put ' inside a string.
 # in oracle, '' is used to put ' inside a string.
-                
-sqlStr = 'update eLogReport set ConfigReportUrl =  \'' + configReportUrl + '\' where RunId = ' + str(runId);
-
 sqlStr = "update eLogReport set %s = '%s' where RunId = %s" % \
-         (urlKey[reportType], reportUrl, runId);
+         (urlKey[reportType], reportUrl, runId)
 
 try:
     c.execute(sqlStr)
