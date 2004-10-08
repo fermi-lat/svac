d700dd011ebc818acb27ed529801d399d67092d3
focke <>
2004-10-08 01:33:53 +0000

    Work towards adding timing stuff,

--- ConfigTables/Src/joboptions.py
+++ ConfigTables/Src/joboptions.py
@@ -3,6 +3,16 @@ outFile = "ConfigTables.html"
 
 calTags = ["log_acpt", "fhe_dac", "fle_dac", "rng_uld_dac", "ref_dac"]
 
+delayTags = ["GLAT/GTEM/cal_trgseq", "GLAT/GTEM/tkr_trgseq", "GLAT/GAEM/trgseq",
+             "GLAT/GTEM/GCCC/alignment", "GLAT/GTEM/GTCC/alignment",
+             "GLAT/GAEM/veto_delay"
+             ]
+stretchTags = ["GLAT/GTEM/GCCC/alignment", "GLAT/GTEM/GCCC/alignment",
+               "GLAT/GTEM/GTCC/alignment", "GLAT/GAEM/GARC/veto_width",
+               "GLAT/GAEM/GARC/hitmap_delay", "GLAT/GAEM/GARC/hitmap_width",
+               "GLAT/GGEM/GGEMW/window_width", 
+               ]
+
 schemaTag = "schema"
 
 # These are things for which the schema should define allowed values for
