65bd6c50d6ee85e7929aecaeea25630dbcedde2e
xchen <>
2004-11-12 23:26:39 +0000

    *** empty log message ***

--- NtupleCompare/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/afs/slac.stanford.edu/g/glast/applications/CMT/v1r16p20040701; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt build temporary_name -quiet`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt -quiet cleanup -sh -pack=NtupleCompare -version=v0 -path=/nfs/farm/g/glast/u06/chen/svac $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
