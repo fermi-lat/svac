69ffcabc294370827ffcac94f3228e5b599fe713
focke <>
2005-03-14 20:42:51 +0000

    Added XML generators (backported from v2 branch).

---
+++ svacPipeline/configReport/genXml.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use lib "$ENV{'svacPlRoot'}/lib";
+use environmentalizer;
+environmentalizer::sourceCsh("$ENV{'svacPlRoot'}/setup/svacPlSetup.cshrc");
+
+my $urlUpdater = $ENV{'urlUpdateWrapper'};
+
+
+my $batchgroup = $ENV{'batchgroup'};
+
+# configReport
+my $configDataDir = "$ENV{'svacHead'}/configReport/$ENV{'configReportVersion'}";
+
+
+my $configReportXml = 
+"<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<pipeline
+    xmlns=\"http://glast-ground.slac.stanford.edu/pipeline\"
+    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+    xsi:schemaLocation=\"http://glast-ground.slac.stanford.edu/pipeline http://glast-ground.slac.stanford.edu/pipeline.xsd\">
+
+    <name>$ENV{'configReportTask'}</name>
+    <type>>Report</type>
+    <dataset-base-path>$configDataDir</dataset-base-path>
+    <run-log-path>/temp/</run-log-path>
+        <executable name=\"ConfigTablesWrapper\" version=\"$ENV{'configReportTaskVersion'}\">
+            $ENV{'configTaskDir'}/ConfigTablesWrapper.pl
+        </executable>
+        <executable name=\"configReportUrlWrapper\" version=\"$ENV{'svacPlLibVersion'}\">
+            $urlUpdater
+        </executable>
+
+        <batch-job-configuration name=\"express-job\" queue=\"express\" group=\"$batchgroup\">
+            <working-directory>$configDataDir</working-directory>
+            <log-file-path>$configDataDir</log-file-path>
+        </batch-job-configuration>
+        <batch-job-configuration name=\"short-job\" queue=\"short\" group=\"$batchgroup\">
+            <working-directory>$configDataDir</working-directory>
+            <log-file-path>$configDataDir</log-file-path>
+        </batch-job-configuration>
+
+        <file name=\"tarBall\" type=\"tgz\" file-type=\"Analysis\"/>
+
+        <foreign-input-file name=\"snapshot\" pipeline=\"$ENV{'onlineTask'}\" name=\"snapshot\"/>
+
+        <processing-step name=\"ConfigTables\" executable=\"ConfigTablesWrapper\" batch-job-configuration=\"short-job\">
+                        <input-file name=\"schema\"/>
+                        <input-file name=\"snapshot\"/>
+                        <output-file name=\"tarBall\"/>
+        </processing-step>
+        <processing-step name=\"configReportUrl\" executable=\"configReportUrlWrapper\" batch-job-configuration=\"express-job\">
+                        <input-file name=\"tarBall\"/>
+        </processing-step>
+</pipeline>
+
+";
+
+my $configReportXmlFileName = "$ENV{'configReportTask'}.xml";
+open FIELDS, '>', $configReportXmlFileName;
+print FIELDS $configReportXml;
+close FIELDS;
+
---
+++ svacPipeline/svacTuple/genXml.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use lib "$ENV{'svacPlRoot'}/lib";
+use environmentalizer;
+environmentalizer::sourceCsh("$ENV{'svacPlRoot'}/setup/svacPlSetup.cshrc");
+
+my $urlUpdater = $ENV{'urlUpdateWrapper'};
+
+
+my $batchgroup = $ENV{'batchgroup'};
+
+# svacTuple
+my $svacTupleDataDir = "$ENV{'calibRoot'}/svacRoot/$ENV{'svacTupleVersion'}";
+
+my $svacTupleXml = 
+"<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<pipeline
+    xmlns=\"http://glast-ground.slac.stanford.edu/pipeline\"
+    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+    xsi:schemaLocation=\"http://glast-ground.slac.stanford.edu/pipeline http://glast-ground.slac.stanford.edu/pipeline.xsd\">
+
+    <name>$ENV{'svacTupleTask'}</name>
+    <type>Analysis</type>
+    <dataset-base-path>$svacTupleDataDir</dataset-base-path>
+    <run-log-path>/temp/</run-log-path>
+        <executable name=\"svacTupleWrapper\" version=\"$ENV{'svacTupleTaskVersion'}\">
+            $ENV{'svacTupleTaskDir'}/RunRootAnalyzerWrapper.pl
+        </executable>
+        <executable name=\"svacRootFileWrapper\" version=\"$ENV{'svacPlLibVersion'}\">
+            $urlUpdater
+        </executable>
+
+        <batch-job-configuration name=\"long-job\" queue=\"long\" group=\"$batchgroup\">
+            <working-directory>$svacTupleDataDir</working-directory>
+            <log-file-path>$svacTupleDataDir</log-file-path>
+        </batch-job-configuration>
+        <batch-job-configuration name=\"express-job\" queue=\"express\" group=\"$batchgroup\">
+            <working-directory>$svacTupleDataDir</working-directory>
+            <log-file-path>$svacTupleDataDir</log-file-path>
+        </batch-job-configuration>
+
+        <file name=\"histogram\" type=\"root\" file-type=\"histogram\"/>
+        <file name=\"jobOptions\" type=\"jobOpt\" file-type=\"text\"/>
+        <file name=\"script\" type=\"csh\" file-type=\"script\"/>
+        <file name=\"svac\" type=\"root\" file-type=\"svac\"/>
+
+        <foreign-input-file name=\"digi\" pipeline=\"$ENV{'digitizationTask'}\" file=\"digi\"/>
+        <foreign-input-file name=\"recon\" pipeline=\"$ENV{'reconTask'}\" file=\"recon\"/>
+
+        <processing-step name=\"svacTuple\" executable=\"svacTupleWrapper\" batch-job-configuration=\"long-job\">
+                        <input-file name=\"digi\"/>
+                        <input-file name=\"recon\"/>
+                        <output-file name=\"histogram\"/>
+                        <output-file name=\"svac\"/>
+                        <output-file name=\"script\"/>
+                        <output-file name=\"jobOptions\"/>
+        </processing-step>
+        <processing-step name=\"svacRootFile\" executable=\"svacRootFileWrapper\" batch-job-configuration=\"express-job\">
+                        <input-file name=\"svac\"/>
+        </processing-step>
+</pipeline>
+
+";
+
+my $svacTupleXmlFileName = "$ENV{'svacTupleTask'}.xml";
+open FIELDS, '>', $svacTupleXmlFileName;
+print FIELDS $svacTupleXml;
+close FIELDS;
+
