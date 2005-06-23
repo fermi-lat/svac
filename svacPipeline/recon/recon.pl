51f0461596ad4d42196a778158ac065347ab3a50
cvs2git <>
2005-06-23 22:57:51 +0000

    This commit was manufactured by cvs2svn to create tag 'v3r1p21'.

--- svacPipeline/recon/recon.pl
+++ svacPipeline/recon/recon.pl
@@ -21,10 +21,6 @@ EOT
 my $particleType =  `$ENV{'svacPlLib'}/queryElogReportTable.pl $runId particletype`;
 
 my $tkrSerNo = `$ENV{'svacPlLib'}/queryElogReportTable.pl $runId tkr_ser_no`;
-
-my @tkrSep = ($tkrSerNo =~ m/\?+/g);
-my $nTwr = $#tkrSep + 1;
-
 $tkrSerNo =~ s/\?//g;
 $tkrSerNo =~ s/\n//g;
 $tkrSerNo =~ s/\(.*?\)//g;
@@ -63,24 +59,6 @@ elsif($tkrSerNo && $calSerNo) {
     $instrumentType = $tkrSerNo.':Cal'.$calSerNo;
 }
 
-my @geometries = ('$(XMLGEODBSROOT)/xml/flight/flightSegVols.xml', #  0
-				  '$(XMLGEODBSROOT)/xml/flight/flightSegVols.xml', #  1
-				  '$(XMLGEODBSROOT)/xml/flight/flightSegVols.xml', #  2
-				  '',                                              #  3
-				  '$(XMLGEODBSROOT)/xml/flight/flightSegVols.xml', #  4
-				  '',                                              #  5
-				  '$(XMLGEODBSROOT)/xml/flight/flightSegVols.xml', #  6
-				  '',                                              #  7
-				  '$(XMLGEODBSROOT)/xml/flight/flightSegVols.xml', #  8
-				  '',                                              #  9
-				  '',                                              # 10
-				  '',                                              # 11
-				  '',                                              # 12
-				  '',                                              # 13
-				  '',                                              # 14
-				  '',                                              # 15
-				  '$(XMLGEODBSROOT)/xml/flight/flightSegVols.xml', # 16
-				  );
 
 my $cmtPath = $ENV{'CMTPATH'};
 my $cmtDir = $ENV{'reconCmt'};
@@ -104,16 +82,9 @@ print SHELLFILE "setenv JOBOPTIONS $jobOptionFile \n";
 print SHELLFILE "$exe || exit 1 \n";
 close(SHELLFILE);
 
-my $geoFile = $geometries[$nTwr];
+my $geoFile = '$(XMLGEODBSROOT)/xml/flight/flightSegVols.xml';;
 if($em) {
-	$geoFile = '$(XMLGEODBSROOT)/xml/em2/em2SegVols.xml';
-}
-
-if (!$geoFile) {
-	print STDERR "No geometry for $nTwr towers!\n";
-	exit(1);
-} else {
-	print STDERR "This run has $nTwr towers, using geometry file $geoFile.\n";
+$geoFile = '$(XMLGEODBSROOT)/xml/em2/em2SegVols.xml';
 }
 
 open(JOBOPTIONFILE, ">$jobOptionFile") || die "Can't open $jobOptionFile, abortted!";
