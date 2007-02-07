6c696df8b280926946cbd5d60cfec61e4dac15a4
borgland <>
2007-02-07 18:35:29 +0000

    Fixed typo in ntuple description.

--- EngineeringModelRoot/src/mainpage.h
+++ EngineeringModelRoot/src/mainpage.h
@@ -231,7 +231,7 @@ This means that this time will be later than the time of any event time. </TD> <
 <TR> <TD> ContextLsfTimeTimeTicks </TD> <TD> UInt </TD> <TD> The number of system clock ticks (nominally 50ns) since last the last time hack.  </TD> </TR>
 
 <TR> <TD>  ContextLsfTimeHackHacks </TD> <TD> UInt </TD> <TD> The number of 1-PPS time hacks.  This counter wraps around at 127. </TD> </TR>  
-<TR> <TD>  ContextLsfTimeHackHacks </TD> <TD> UInt </TD> <TD> The value of the LAT system clock at the last 1-PPS time hack. </TD> </TR>  
+<TR> <TD>  ContextLsfTimeHackTicks </TD> <TD> UInt </TD> <TD> The value of the LAT system clock at the last 1-PPS time hack. </TD> </TR>  
 
 <TR> <TD> ContextRunType </TD> <TD> Int  </TD> <TD> Which type of run was this, particle data or charge injection. </TD> </TR>
 
