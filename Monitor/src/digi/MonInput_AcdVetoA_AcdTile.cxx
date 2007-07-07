38d6764a3c1aa2453cb181d0bfa415791a1a95c4
dpaneque <>
2007-07-07 21:14:54 +0000

    Use function AcdId::gemIndexFromTile to get tile id, instead of 'custom' function.

--- Monitor/src/digi/MonInput_AcdVetoA_AcdTile.cxx
+++ Monitor/src/digi/MonInput_AcdVetoA_AcdTile.cxx
@@ -5,6 +5,7 @@
 // Object created automatically by script makeNewMonObject.pl
 //
 #include "MonInput_AcdVetoA_AcdTile.h"
+#include "commonRootData/idents/AcdId.h"
 #include <iostream>
 
 // User defined part 
@@ -53,33 +54,22 @@ void MonInput_AcdVetoA_AcdTile::setValue(TObject* event) {
     const AcdDigi* acdDigi = dynamic_cast<const AcdDigi*>(acdDigiCol->At(iDigi));
     assert(acdDigi);
 
-    UShort_t AcdGemID = getGemId ( acdDigi->getId().getId() );
-    if(acdDigi->getVeto(AcdDigi::A))
-     if(AcdGemID>=0&&AcdGemID<128) m_val[AcdGemID]++;
-   
+    UShort_t AcdGemID = AcdId::gemIndexFromTile(acdDigi->getId().getId());
+    if(acdDigi->getVeto(AcdDigi::A)){
+      if(AcdGemID>=0&&AcdGemID<128) m_val[AcdGemID]++;
+      else{
+	if(acdDigi->getId().getId() != 899)// NA values are set to 899
+	  {
+	    std::cout << " MonInput_AcdVetoA_AcdTile::setValue: WARNING" << std::endl
+		      << " AcdGemID = " <<  AcdGemID << std::endl
+		      << " acdDigi->getId().getId() = " << acdDigi->getId().getId()
+		      << " , which is NOT the conventional NA value (=899)"
+		      << std::endl;
+	  }
+      }
+    }
   }
 }
-
-
-
-UShort_t MonInput_AcdVetoA_AcdTile::getGemId(UInt_t id) {
-  UInt_t face = id / 100;
-  UInt_t row = (id % 100 ) / 10;
-  UInt_t col = (id % 10 );
-  switch ( face ) {
-  case 0:  return 64 + 5*row + col;  // top:      64 - 89
-  case 1:  return 32 + 5*row + col;  // -x side:  32 - 47
-  case 2:  return      5*row + col;  // -y side    0 - 15
-  case 3:  return 48 + 5*row + col;  // +x side   48 - 63
-  case 4:  return 16 + 5*row + col;  // +y side   16 - 31
-  case 5:                                   // x ribbons 96 - 99
-    return 96 + col;
-  case 6:                                   // y ribbons 100- 103
-    return 100 + col;
-  }
-  return 0xFFFF;
-}
-
 
 
 std::string MonInput_AcdVetoA_AcdTile::getInputSource(){
