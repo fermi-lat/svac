4b7af5a90a30f06eb720a03324aa193bd8feae5f
cvs2git <>
2007-09-21 21:22:15 +0000

    This commit was manufactured by cvs2svn to create tag 'v3r2'.

--- TestReport/pl/GenTestReport.pl
+++ TestReport/pl/GenTestReport.pl
@@ -34,14 +34,12 @@ my $pdfFile = 'refman.pdf';
 my $htmlFile = 'index.html';
 my $pwd =  $ENV{PWD};
 
-my $appRoot = '/nfs/farm/g/glast/u33/borgland/Releases/EngineeringModel-v8r1109p10/Code-opt/svac/';
-my $testReportVersion = 'v3r7p5';
-my $emVersion = 'v8r1109p10';
-my $cmtPath = "$appRoot:/nfs/farm/g/glast/u30/builds/rh9_gcc32opt/EngineeringModel/EngineeringModel-$emVersion";
-my $cmtDir = "$appRoot/TestReport/$testReportVersion/cmt";
-my $exeDir = "$appRoot/TestReport/$testReportVersion/rh9_gcc32opt";
-my $doxyFile = "$appRoot/TestReport/$testReportVersion/src/ReportDoxyfile";
-my $latexHeader = "$appRoot/TestReport/$testReportVersion/src/latexHeader.tex";
+my $appRoot = '/nfs/farm/g/glast/u06/chen/svac';
+my $cmtPath = "$appRoot:/nfs/farm/g/glast/u05/builds/rh9_gcc32/EngineeringModel/EngineeringModel-v3r0406p1";
+my $cmtDir = "$appRoot/TestReport/v3r2/cmt";
+my $exeDir = "$appRoot/TestReport/v3r2/rh9_gcc32";
+my $doxyFile = "$appRoot/TestReport/v3r2/src/ReportDoxyfile";
+my $latexHeader = "$appRoot/TestReport/v3r2/src/latexHeader.tex";
 
 my $shellFile = $reportDir.'/'.$prefix.'_report.scr';
 open(SHELLFILE, ">$shellFile") || die "Can't open $shellFile, abortted!";
