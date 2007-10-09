aaf5571c25cdd192db29a6ea5eaed688d5182718
dpaneque <>
2007-10-09 03:47:12 +0000

    Value set to -1 if Recon Energy is 0.0

--- Monitor/src/cal/MonInput_CalXtalFaceSignal.cxx
+++ Monitor/src/cal/MonInput_CalXtalFaceSignal.cxx
@@ -16,7 +16,7 @@
 // #define ACCESSOR CalXtalFaceSignal
 #define MONSOURCE CalEvent
 #define INPUTSOURCE "CalEvent"
-#define DESCRIPTION "Vector [tower][callayer][calcolumn][xface] reporting the reconstructed energy (MeV) for the xtal log and face defined by the coordinates  tower, callayer, calcolumn, xface."
+#define DESCRIPTION "Vector [tower][callayer][calcolumn][xface] reporting the reconstructed energy (MeV) for the xtal log and face defined by the coordinates  tower, callayer, calcolumn, xface. If reconstruction was not possible, the value (for that tower,callayer,calcolumn,xface) is set to -1.0."
 
 // End user defined part 
 
@@ -33,6 +33,20 @@ int MonInput_CalXtalFaceSignal::setOutputBranch(TTree* tree) {
 void MonInput_CalXtalFaceSignal::enableInputBranch(TTree& tree){
   tree.SetBranchStatus(INBRANCH,1);
   tree.SetBranchAddress(INBRANCH,m_val);
+
+  // check that it contains something; if not, set to -1.0
+  
+  const UShort_t ntower(16),ncallayer(8),ncalcolumn(12), nface(2);
+  for(UShort_t tower = 0; tower<ntower;tower++){
+    for(UShort_t callayer=0;callayer<ncallayer;callayer++){
+      for(UShort_t calcolumn=0;calcolumn<ncalcolumn;calcolumn++){
+	for(UShort_t face=0;face<nface;face++){
+	  if(m_val[tower][callayer][calcolumn][face] == 0.0000)
+	    m_val[tower][callayer][calcolumn][face] = -1.0;
+	}
+      }
+    }
+  }
 }
 void MonInput_CalXtalFaceSignal::setValue(TObject* event) {
 
