20fda4770d1f830132d14a2f60d72c92ede91890
dpaneque <>
2008-03-02 07:11:59 +0000

    Corrected bug introduced on Feb 26th

--- Monitor/src/makeNewMonObject.pl
+++ Monitor/src/makeNewMonObject.pl
@@ -200,7 +200,7 @@ print CFILE "//
 
 #define NAME $name
 #define OUTBRANCH \"$name\"
-#define LEAF \"$inbranch/$ntype\"
+#define LEAF \"$name/$ntype\"
 #define INBRANCH \"$inbranch\"
 #define ACCESSOR $access
 #define MONSOURCE $monsource
