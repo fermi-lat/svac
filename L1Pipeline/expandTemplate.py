e4120e6bfabb52f1acbb8afdbfd0f9ed90856aee
cvs2git <>
2008-04-11 22:12:42 +0000

    This commit was manufactured by cvs2svn to create tag 'v3r13'.

--- L1Pipeline/expandTemplate.py
+++
@@ -1,8 +0,0 @@
-#!/afs/slac/g/glast/isoc/flightOps/rhel3_gcc32/ISOC_PROD/bin/shisoc python2.5
-
-
-def expand(inFile, outFile, dic):
-    inData = open(inFile).read()
-    outData = inData % dic
-    open(outFile, 'w').write(outData)
-    return
