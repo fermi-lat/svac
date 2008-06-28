e3c8c6a9098a2f215985efac24af77750d53367b
cvs2git <>
2008-06-28 01:02:22 +0000

    This commit was manufactured by cvs2svn to create tag 'v6r12'.

--- L1Pipeline/retireRun.py
+++
@@ -1,3 +0,0 @@
-# This script is automagically pasted into the XML at install time
-print '''This should mark datasets from run %s as %s,
-but instead it prints this message.'''  % (RUNID, RUNSTATUS)
