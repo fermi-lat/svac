15a71b8b9837f0bec59789949d9f46273d7a9753
bregeon <>
2009-08-05 15:16:01 +0000

    FastMon spacecraft distance to SAA - GDQMQ-311

---
+++ Monitor/src/fastmon/MonInput_FastMon_spacecraft_distance_to_saa.h
@@ -0,0 +1,35 @@
+// 
+// Class for input of quantity FastMon_spacecraft_distance_to_saa for monitoring 
+// 
+// Created by bregeon on Wed Aug  5 13:14:06 2009 
+// Object created automatically by script makeNewMonObject.pl
+//
+#ifndef MonInput_FastMon_spacecraft_distance_to_saa_h
+#define MonInput_FastMon_spacecraft_distance_to_saa_h
+#include "../MonInputObject.h"
+#include "Rtypes.h"
+#include "TTree.h"
+#include "TObject.h"
+
+// user defined part
+
+#define VARTYPE Double_t 
+
+// end user defined part
+
+class MonInput_FastMon_spacecraft_distance_to_saa: public MonInputObject{
+ public:
+  MonInput_FastMon_spacecraft_distance_to_saa();
+  ~MonInput_FastMon_spacecraft_distance_to_saa();
+  int setOutputBranch(TTree*);
+  void enableInputBranch(TTree&);
+  void setValue(TObject* event) ;
+  std::string getInputSource();
+  std::string getDescription();
+ private:
+  VARTYPE m_val;
+};
+
+#undef VARTYPE
+#endif
+
