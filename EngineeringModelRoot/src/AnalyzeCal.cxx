6d17e9e8e161c6300de6f59a4c0fdc4a756bf062
cvs2git <>
2009-09-25 22:23:26 +0000

    This commit was manufactured by cvs2svn to create tag 'v1r3p3'.

--- EngineeringModelRoot/src/AnalyzeCal.cxx
+++ EngineeringModelRoot/src/AnalyzeCal.cxx
@@ -31,6 +31,25 @@ int main(int argc, char** argv)
 
   TFile f(outputFile.c_str(), "RECREATE");
 
+  TNtuple tuple("tuple", "tuple", "event:layer:col:side:range:adc");
+
+  //  int nLayers = 8, nCols = 12, nSides = 2, nRanges = 4;
+
+  /*
+  TObjArray* rawAdcHist = new TObjArray(nLayers*nCols*nSides);
+
+  for(int iLayer = 0; iLayer != nLayers; ++iLayer) {
+    for(int iCol = 0; iCol != nCols; ++iCol) {
+      for(int iSide = 0; iSide != nSides; ++iSide) {
+	  char rawAdcName[] = "adc0000";
+	  sprintf(rawAdcName, "adc%1d%02d%1d", iLayer, iCol, iSide);
+	  rawAdcHist->AddAt(new TH1F(rawAdcName, rawAdcName,300, 0, 4200),
+			    iLayer*nCols*nSides + iCol*nSides + iSide);
+      }
+    }
+  }
+  */
+
   TFile m_digiFile(digiFile.c_str(), "READ");
   if(m_digiFile.IsZombie()) {
     cout << "digi file not found" << endl;
@@ -50,20 +69,33 @@ int main(int argc, char** argv)
 
     m_digiBranch->GetEntry(iEvent);
 
-    int id = m_digiEvent->getEventId();
+    const TObjArray* calDigiCol = m_digiEvent->getCalDigiCol();
+    if (!calDigiCol) continue;
 
-    if(id == 4542) {
-      const TObjArray* calDigiCol = m_digiEvent->getCalDigiCol();
+    int nCalDigi = calDigiCol->GetLast()+1;
 
-      if (!calDigiCol) continue;
+    for(int iDigi = 0; iDigi != nCalDigi; ++iDigi) {
 
-      int nCalDigi = calDigiCol->GetLast()+1;
+      const CalDigi* calDigi = dynamic_cast<const CalDigi*>(calDigiCol->At(iDigi));
 
-      cout << "nCalDigi = " << nCalDigi << endl;
+      assert(calDigi != 0);
+
+      const CalXtalId id = calDigi->getPackedId();
+      int layer = id.getLayer();
+      int col = id.getColumn();
+
+      //      ((TH1F*) rawAdcHist->At(layer*nCols*nSides + col*nSides))->Fill(calDigi->getAdc(0, CalXtalId::NEG));
+
+      //      ((TH1F*) rawAdcHist->At(layer*nCols*nSides + col*nSides + 1))->Fill(calDigi->getAdc(0, CalXtalId::POS));
+
+      tuple.Fill(iEvent, layer, col, 0, 0, calDigi->getAdc(0, CalXtalId::NEG));
+      tuple.Fill(iEvent, layer, col, 1, 0, calDigi->getAdc(0, CalXtalId::POS));
 
     }
   }
  
-
+  f.cd();
+  //  rawAdcHist->Write();
+  tuple.Write();
 }
 
