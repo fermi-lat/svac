d177a06e3723fc99bc971c3055c07492657a10f0
monzani <>
2012-06-20 20:24:27 +0000

    added leap second from June 30th, 2012

--- TestReport/src/RunVerify.cxx
+++ TestReport/src/RunVerify.cxx
@@ -226,6 +226,8 @@ int RunVerify::analyzeDigi(const char* digiFileName="digi.root", bool completeRu
     if (timeStamp > 157766400) deltaTimeUgly--; 
     // Leap second from Dec 31st, 2008
     if (timeStamp > 252460800) deltaTimeUgly--;
+    // Leap second from Jun 30th, 2012
+    if (timeStamp > 362793601) deltaTimeUgly--
     timeStamp = timeStamp + deltaTimeUgly;
 
     if ( ccsdsTime < timeStamp ){
