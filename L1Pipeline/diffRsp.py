fda08738fc4992816fff9df56873dfeb2baa856e
monzani <>
2015-01-30 03:35:29 +0000

    skipping diffuse response

--- L1Pipeline/diffRsp.py
+++ L1Pipeline/diffRsp.py
@@ -43,15 +43,17 @@ def diffRsp(files, inFileTypes, outFileTypes, workDir, **args):
     source %(stSetup)s
     ''' % locals()
 
-    for evcls in config.diffRspMap.keys():
-        irf = config.diffRspMap[evcls]['irf']
-        model = config.diffRspMap[evcls]['model']
-        cmdTail = '''%(app)s scfile=%(stagedFt2File)s evfile=%(tmpFt1File)s srcmdl=%(model)s irfs=%(irf)s evclass=%(evcls)s
-        ''' % locals()
-        cmd = cmdHead + cmdTail
-        status |= runner.run(cmd)
-        if status: return status
-        continue
+    #do nothing here: there is no diffuse responseto be computed for Pass8
+    #just rename the file (see below) and be happy
+    #for evcls in config.diffRspMap.keys():
+        #irf = config.diffRspMap[evcls]['irf']
+        #model = config.diffRspMap[evcls]['model']
+        #cmdTail = '''%(app)s scfile=%(stagedFt2File)s evfile=%(tmpFt1File)s srcmdl=%(model)s irfs=%(irf)s evclass=%(evcls)s
+        #''' % locals()
+        #cmd = cmdHead + cmdTail
+        #status |= runner.run(cmd)
+        #if status: return status
+        #continue
 
     os.rename(tmpFt1File, stagedOutFile)
 
