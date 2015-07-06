b252a2dd9f307535daef6b09e86e21aab06dc622
cvs2git <>
2015-07-06 20:35:14 +0000

    This commit was manufactured by cvs2svn to create tag 'Monitor-03-14-01'.

--- L1Pipeline/expandTemplate.py
+++
@@ -1,8 +0,0 @@
-#!/afs/slac/g/glast/isoc/flightOps/rhel6_gcc44/ISOC_PROD/bin/shisoc python2.6
-
-
-def expand(inFile, outFile, dic):
-    inData = open(inFile).read()
-    outData = inData % dic
-    open(outFile, 'w').write(outData)
-    return
